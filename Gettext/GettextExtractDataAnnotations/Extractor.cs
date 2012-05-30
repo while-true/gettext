6879d8857ec67f552e19417d891ff765a2ae5c69
Rudi Benkovic <rudi@whiletrue.com>
2012-05-30 11:50:38 +0200

    libs, data annotations extractor app

---
+++ Gettext/GettextExtractDataAnnotations/Extractor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GettextExtractDataAnnotations
+{
+    public class Extractor
+    {
+        public Extractor()
+        {
+            translations = new List<TranslationString>();
+        }
+
+        public void Parse(Assembly assembly)
+        {
+            var types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Console.WriteLine("Type {0}", type.Name);
+
+                Parse(type);
+
+                var props = type.GetProperties();
+                foreach (var propertyInfo in props)
+                {
+                    Parse(propertyInfo);
+                }
+
+                
+            }
+        }
+
+        public string ToPoString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var translationString in translations)
+            {
+                sb.AppendFormat("msgid \"{0}\"\n", Escape(translationString.Str));
+                sb.AppendFormat("msgstr \"\"\n");
+                sb.Append("\n");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string Escape(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return string.Empty;
+            return str.Replace("\"", "\\\"");
+        }
+
+        private List<TranslationString> translations; 
+
+        private class TranslationString
+        {
+            public string Str { get; set; }
+
+            public TranslationString(string str)
+            {
+                Str = str;
+            }
+        }
+
+        private void AddTranslation(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return;
+            translations.Add(new TranslationString(str));
+        }
+
+        private void Parse(MemberInfo info)
+        {
+            var d = info.GetCustomAttributes(true);
+            if (d.Length == 0) return;
+
+            foreach (var attribute in d)
+            {
+                var displayAttribute = attribute as DisplayAttribute;
+                if (displayAttribute != null)
+                {
+                    AddTranslation(displayAttribute.Name);
+                }
+
+                var va = attribute as ValidationAttribute;
+                if (va != null)
+                {
+                    AddTranslation(va.ErrorMessage);
+                }
+            }
+        }
+    }
+}
---
+++ Gettext/GettextExtractorApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using GettextExtractDataAnnotations;
+
+namespace GettextExtractorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Missing directory");
+                return;
+            }
+
+            var d = args[0];
+            if (string.IsNullOrWhiteSpace(d)) throw new Exception("Missing directory specification");
+            var info = new DirectoryInfo(d);
+            
+            var files = Directory.GetFiles(info.FullName, "*.dll", SearchOption.AllDirectories);
+
+            var ext = new Extractor();
+
+            foreach (var file in files)
+            {
+                
+                try
+                {
+                    //var a = Assembly.ReflectionOnlyLoadFrom(file);
+                    
+                    //var dd = AppDomain.CreateDomain("parse");
+                    //var a = dd.Load(File.ReadAllBytes(file));
+
+                    var a = Assembly.LoadFile(file);
+
+                    ext.Parse(a);
+
+                    Console.WriteLine("Parsed {0}", file);
+
+                } catch(Exception e)
+                {
+                    Console.WriteLine("Error parsing {0}.\n{1}\n", file, e.Message);
+                }
+            }
+
+            File.WriteAllText("messages.po", ext.ToPoString(), Encoding.UTF8);
+            Console.WriteLine("Wrote messages.po");
+
+        }
+    }
+}
