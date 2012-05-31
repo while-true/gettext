f49e1b9b8a79d7c4c7ed7737e2b4b8770df5afd2
Rudi Benkovic <rudi@whiletrue.com>
2012-05-31 15:55:51 +0200

    expression tests

--- Gettext/GettextTests/TestPoParser.cs
+++ Gettext/GettextTests/TestPoParser.cs
@@ -51,5 +51,74 @@ namespace GettextTests
                 }
             } while (tok > (int)GettextLib.Parser.Tokens.EOF);
         }
+
+        [Test]
+        public void TestExpressionParser()
+        {
+            var expressions = new[]
+                               {
+"nplurals=2;",
+"nplurals=2; plural=n == 1 ? 0 : 1;",
+"nplurals=1; plural=0;",
+"nplurals=2; plural=n != 1;",
+"nplurals=2; plural=n>1;",
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2;",
+"nplurals=3; plural=n==1 ? 0 : n==2 ? 1 : 2;",
+"nplurals=3; plural=n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2;",
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2;",
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;",
+"nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;",
+"nplurals=3; plural=n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;",
+"nplurals=4; plural=n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3;"
+                                  };
+
+
+
+
+            foreach (var expression in expressions)
+            {
+                Console.WriteLine(expression);
+                Console.WriteLine("====");
+
+                //ScannerEvalDump(expression);
+
+                {
+                    var sc = new GettextLib.ExpressionEvaluator.Scanner();
+                    sc.SetSource(expression, 0);
+
+                    var p = new GettextLib.ExpressionEvaluator.Parser(sc);
+
+                    p.Parse();
+
+                    var scr = p.Script;
+                    Console.WriteLine(scr.ToPrint());
+                }
+
+
+                Console.WriteLine("====");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
+        }
+
+        private void ScannerEvalDump(string str)
+        {
+            var scanner = new GettextLib.ExpressionEvaluator.Scanner();
+            scanner.SetSource(str, 0);
+
+            int tok;
+            do
+            {
+                tok = scanner.yylex();
+                if (Enum.IsDefined(typeof(GettextLib.ExpressionEvaluator.Tokens), tok))
+                {
+                    var t = (GettextLib.ExpressionEvaluator.Tokens)tok;
+
+                    Console.WriteLine("Token " + t);
+                }
+            } while (tok > (int)GettextLib.ExpressionEvaluator.Tokens.EOF);
+        }
     }
 }
