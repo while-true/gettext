d7e47f08bf301580081b45c2a29b197e0ddb2dff
Rudi Benkovic <rudi@whiletrue.com>
2012-06-18 10:53:42 +0200

    expanded formats...

--- Gettext/GettextMvcLib/StringFormatWith.cs
+++ Gettext/GettextMvcLib/StringFormatWith.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Web.UI;
 using GettextLib;
+using System.Web;
+using System.Globalization;
+using System.Web.Mvc;
 
 namespace GettextMvcLib
 {
@@ -33,6 +36,49 @@ namespace GettextMvcLib
             });
 
             return string.Format(format.CultureInfo, rewrittenFormat, values.ToArray());
+        }
+
+        /// <summary>
+        /// Calls String.Format using the supplied arguements 
+        /// </summary>
+        /// <param name="format">The object being formatted</param>
+        /// <param name="args">strings that will replace formatting characters</param>
+        /// <returns>a string with all formatting characters replaced by the passed in arguements</returns>
+        public static string FormatWith(this GettextTranslatedString format, params string[] args)
+        {
+            if (format.String == null)
+                throw new ArgumentNullException("format");
+
+            return string.Format(format.CultureInfo, format, args);
+        }
+    
+        /// <summary>
+        /// Converts a regular string to an Html encoded string
+        /// </summary>
+        /// <param name="format">The object being formatted</param>
+        /// <param name="htmlEncode">Passing any bool indicates the object should html encoded</param>
+        /// <returns>an HtmlString version of the original string</returns>
+        public static MvcHtmlString HtmlFormat(this GettextTranslatedString format)
+        {
+            if (format.String == null)
+                throw new ArgumentNullException("format");
+
+            return MvcHtmlString.Create(format);
+        }
+        /// <summary>
+        /// Calls String.Format using the supplied arguements 
+        /// to return an Html encoded string
+        /// </summary>
+        /// <param name="format">The object being formatted</param>
+        /// <param name="htmlEncode">Passing any bool indicates the object should html encoded</param>
+        /// <param name="args">objects that may not be strings that will replace formatting characters</param>
+        /// <returns>an HtmlString with all formatting characters replaced by the passed in arguements</returns>
+        public static MvcHtmlString HtmlFormatWith(this GettextTranslatedString format, params string[] args)
+        {
+            if (format.String == null)
+                throw new ArgumentNullException("format");
+
+            return MvcHtmlString.Create(string.Format(format.CultureInfo, format, args));
         }
     }
 }
