39f3f0beb483fe177cb1f8f497e4bb8b9cc999ca
Rudi Benkovic <rudi@whiletrue.com>
2012-06-18 11:15:27 +0200

    updated build ... headers

--- Gettext/GettextTools/extract_vars.bat
+++ Gettext/GettextTools/extract_vars.bat
@@ -1,2 +1,2 @@
 @echo off
-SET xgettext_options=--from-code=UTF-8 --language=C# -k --keyword=_ --keyword=_:1,2 --keyword=PGettext:1c,2 --keyword=PNGettext:1c,2,3 --omit-header
+SET xgettext_options=--from-code=UTF-8 --language=C# -k --keyword=_ --keyword=_:1,2 --keyword=PGettext:1c,2 --keyword=PNGettext:1c,2,3
