2074eec0da1bb50292e4721872d73e344ad625f6
Rudi Benkovic <rudi@whiletrue.com>
2012-07-26 15:10:46 +0200

    added copyrights removed test code...

--- Gettext/GettextTests/TestPoParser.cs
+++ Gettext/GettextTests/TestPoParser.cs
@@ -14,7 +14,7 @@ namespace GettextTests
     public class TestPoParser
     {
         [Test]
-        public void Test1()
+        public void TestParser()
         {
             var d = new DirectoryInfo("test-po");
             var files = d.GetFiles("*.po");
@@ -37,24 +37,6 @@ namespace GettextTests
                     Assert.That(t.Fuzzy, Is.True);
                 }
             }
-        }
-
-        private void ScannerDump(string str)
-        {
-            var scanner = new GettextLib.Parser.Scanner();
-            scanner.SetSource(str, 0);
-
-            int tok;
-            do
-            {
-                tok = scanner.yylex();
-                if (Enum.IsDefined(typeof(GettextLib.Parser.Tokens), tok))
-                {
-                    var t = (GettextLib.Parser.Tokens)tok;
-
-                    Console.WriteLine("Token " + t);
-                }
-            } while (tok > (int)GettextLib.Parser.Tokens.EOF);
         }
 
         [Test]
@@ -125,88 +107,7 @@ namespace GettextTests
             }
 
         }
-
-        [Test]
-        public void TestExpressionParser()
-        {
-            var expressions = new[]
-                               {
-                                   /*
-"nplurals=2;",
-"nplurals=2; plural=n == 1 ? 0 : 1;",
-"nplurals=1; plural=0;",
-"nplurals=2; plural=n != 1;",
-"nplurals=2; plural=n>1;",
-"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2;",
-"nplurals=3; plural=n==1 ? 0 : n==2 ? 1 : 2;",
-"nplurals=3; plural=n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2;",
-"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2;",
-"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;",
-"nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;",
-"nplurals=3; plural=n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;",
-"nplurals=4; plural=n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3;"
-                                    */
-
-
-                                   "plural= n%100==1 ? 0 : n%100==2 ? 1 : 3;"
-
-                                  };
-
-
-
-
-            foreach (var expression in expressions)
-            {
-                Console.WriteLine(expression);
-                Console.WriteLine("====");
-
-                //ScannerEvalDump(expression);
-
-                {
-                    var sc = new Scanner();
-                    sc.SetSource(expression, 0);
-
-                    var p = new Parser(sc);
-
-                    p.Parse();
-
-                    var scr = p.Script;
-                    Console.WriteLine(scr.ToPrint());
-
-                    var s = new ExpressionState();
-                    s.SetVar("n", 2);
-                    scr.Execute(s);
-                    Console.WriteLine(s.PrintState());
-                }
-
-
-                Console.WriteLine("====");
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-
-            
-        }
         
-        private void ScannerEvalDump(string str)
-        {
-            var scanner = new GettextLib.ExpressionEvaluator.Scanner();
-            scanner.SetSource(str, 0);
-
-            int tok;
-            do
-            {
-                tok = scanner.yylex();
-                if (Enum.IsDefined(typeof(GettextLib.ExpressionEvaluator.Tokens), tok))
-                {
-                    var t = (GettextLib.ExpressionEvaluator.Tokens)tok;
-
-                    Console.WriteLine("Token " + t);
-                }
-            } while (tok > (int)GettextLib.ExpressionEvaluator.Tokens.EOF);
-        }
-
-
         [Test]
         public void TestPluralTranslations()
         {
