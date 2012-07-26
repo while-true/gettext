a09948061a0de7f30830fa63b571e634ca8bef3b
Rudi Benkovic <rudi@whiletrue.com>
2012-07-26 15:30:06 +0200

    missing file

---
+++ GettextLib/GettextUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace GettextLib
+{
+    public static class GettextUtils
+    {
+        public static string PseudoTranslate(string str)
+        {
+            return string.Format("[!{0}!]", str);
+        }
+        
+
+        public static GettextTranslationContext CreateNullContext()
+        {
+            return new GettextTranslationContext(new LanguageTranslation
+            {
+                Culture = CultureInfo.InvariantCulture,
+                Gettext = new GettextDummy(),
+                LangId = GettextConsts.GettextNullLanguage
+            });
+        }
+    }
+}
