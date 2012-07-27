b94063db90f924dbb193e8526c22b75009377baf
Rudi Benkovic <rudi@whiletrue.com>
2012-07-27 15:42:46 +0200

    named formats in non-web code. copied microsoft code, have to rewrite.

---
+++ GettextTests/TestFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GettextLib;
+using NUnit.Framework;
+
+namespace GettextTests
+{
+    [TestFixture]
+    public class TestFormat
+    {
+        [Test]
+        public void T()
+        {
+            var g = new GettextTranslatedString("Hello {world}!").FormatWithNamed(new {world = "WORLD"});
+            StringAssert.AreEqualIgnoringCase("Hello WORLD!", g);
+        }
+    }
+}
