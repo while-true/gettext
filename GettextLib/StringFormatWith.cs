464d8ad1341b38dbbde6f8c9328132b21b428a6f
Rudi Benkovič <rudi@jawmail.org>
2012-07-28 15:57:23 +0200

    custom code for evals

--- GettextLib/StringFormatWith.cs
+++ GettextLib/StringFormatWith.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using JetBrains.Annotations;
 
 namespace GettextLib
 {
     public static class StringFormatWith
     {
         /// <summary>
-        ///
+        /// Calls String.Format with the current translation string's locale.
         /// </summary>
         /// <param name="format">The string being formatted</param>
         /// <param name="args">strings that will replace formatting characters</param>
@@ -28,6 +23,14 @@ namespace GettextLib
             return string.Format(format.CultureInfo, format, args);
         }
 
+        /// <summary>
+        /// Calls String.Format with the current translation string's locale.
+        /// 
+        /// Uses named parameters.
+        /// </summary>
+        /// <param name="format">String format</param>
+        /// <param name="source">Data object</param>
+        /// <returns>Formatter string</returns>
         public static string FormatWithNamed(this GettextTranslatedString format, object source)
         {
             if (format.String == null)
@@ -45,7 +48,7 @@ namespace GettextLib
 
                 values.Add((propertyGroup.Value == "0")
                   ? source
-                  : DataBinder.Eval(source, propertyGroup.Value));
+                  : StringEval.Eval(source, propertyGroup.Value));
 
                 return new string('{', startGroup.Captures.Count) + (values.Count - 1) + formatGroup.Value + new string('}', endGroup.Captures.Count);
             });
@@ -53,406 +56,41 @@ namespace GettextLib
             return string.Format(format.CultureInfo, rewrittenFormat, values.ToArray());            
         }
 
-        public sealed class DataBinder
+        public static class StringEval
         {
-            private static readonly char[] expressionPartSeparator = new char[] { '.' };
-            private static readonly char[] indexExprStartChars = new char[] { '[', '(' };
-            private static readonly char[] indexExprEndChars = new char[] { ']', ')' };
-            private static readonly ConcurrentDictionary<Type, PropertyDescriptorCollection> propertyCache = new ConcurrentDictionary<Type, PropertyDescriptorCollection>();
-            // By default the new caching behavior will be on.
-            private static bool enableCaching = true;
-
-            // Global overrride switch to enable old behavior.
-            public static bool EnableCaching
+            public static object Eval(object obj, [NotNull] string expr)
             {
-                get
+                if (string.IsNullOrWhiteSpace(expr)) throw new ArgumentNullException("expr");
+                expr = expr.Trim();
+                
+                if (obj == null) return null;
+
+                object ret = obj;
+
+                foreach (var part in expr.Split('.').Select(x => x.Trim()))
                 {
-                    return enableCaching;
-                }
-                set
-                {
-                    enableCaching = value;
-                    if (!value)
+                    if (ret == null) return null;
+
+                    var type = ret.GetType();
+                    var p = type.GetProperty(part);
+                    if (p != null)
                     {
-                        // Clear the cache when caching is turned off.
-                        propertyCache.Clear();
+                        ret = p.GetValue(ret, null);
+                        continue;
                     }
-                }
-            }
-
-            /// <internalonly/> 
-            //
-            public DataBinder()
-            {
-            }
-
-
-            /// <devdoc> 
-            ///    <para>Evaluates data binding expressions at runtime. While
-            ///       this method is automatically called when you create data bindings in a RAD 
-            ///       designer, you can also use it declaratively if you want to simplify the casting 
-            ///       to a text string to be displayed on a browser. To do so, you must place the
-            ///       &lt;%# and %&gt; tags, which are also used in standard ASP.NET data binding, around the data binding expression.</para> 
-            ///    <para>This method is particularly useful when data binding against controls that
-            ///       are in a templated list.</para>
-            ///    <note type="caution">
-            ///       Since this method is called at runtime, it can cause performance 
-            ///       to noticeably slow compared to standard ASP.NET databinding syntax.
-            ///       Use this method judiciously. 
-            ///    </note> 
-            /// </devdoc>
-            public static object Eval(object container, string expression)
-            {
-                if (expression == null)
-                {
-                    throw new ArgumentNullException("expression");
-                }
-
-                expression = expression.Trim();
-
-                if (expression.Length == 0)
-                {
-                    throw new ArgumentNullException("expression");
-                }
-
-                if (container == null)
-                {
-                    return null;
-                }
-
-                string[] expressionParts = expression.Split(expressionPartSeparator);
-
-                return DataBinder.Eval(container, expressionParts);
-            }
-
-
-            /// <devdoc>
-            /// </devdoc> 
-            private static object Eval(object container, string[] expressionParts)
-            {
-                Debug.Assert((expressionParts != null) && (expressionParts.Length != 0),
-                             "invalid expressionParts parameter");
-
-                object prop;
-                int i;
-
-                for (prop = container, i = 0; (i < expressionParts.Length) && (prop != null); i++)
-                {
-                    string expr = expressionParts[i];
-                    bool indexedExpr = expr.IndexOfAny(indexExprStartChars) >= 0;
-
-                    if (indexedExpr == false)
+                    
+                    var f = type.GetField(part);
+                    if (f != null)
                     {
-                        prop = DataBinder.GetPropertyValue(prop, expr);
+                        ret = f.GetValue(ret);
+                        continue;
                     }
-                    else
-                    {
-                        prop = DataBinder.GetIndexedPropertyValue(prop, expr);
-                    }
+                    
+                    throw new Exception(string.Format("Property or field \"{0}\" not found!", part));
                 }
 
-                return prop;
-            }
-
-
-            /// <devdoc>
-            ///    <para> Evaluates data binding expressions at runtime and
-            ///       formats the output as text to be displayed in the requesting browser. While this
-            ///       method is automatically called when you create data bindings in a RAD designer, 
-            ///       you can also use it declaratively if you want to simplify the casting to a text
-            ///       string to be displayed on a browser. To do so, you must place the &lt;%# and %&gt; tags, which are also used in standard ASP.NET data binding, around 
-            ///       the data binding expression.</para> 
-            ///    <para>This method is particularly useful when data binding against controls that
-            ///       are in a templated list.</para> 
-            ///    <note type="caution">
-            ///       Since this method is called at
-            ///       runtime, it can cause performance to noticeably slow compared to standard ASP.NET
-            ///       databinding syntax. Use this method judiciously, particularly when string 
-            ///       formatting is not required.
-            ///    </note> 
-            /// </devdoc> 
-            public static string Eval(object container, string expression, string format)
-            {
-                object value = DataBinder.Eval(container, expression);
-
-                if ((value == null) || (value == System.DBNull.Value))
-                {
-                    return String.Empty;
-                }
-                else
-                {
-                    if (String.IsNullOrEmpty(format))
-                    {
-                        return value.ToString();
-                    }
-                    else
-                    {
-                        return String.Format(format, value);
-                    }
-                }
-            }
-
-            internal static PropertyDescriptorCollection GetPropertiesFromCache(object container)
-            {
-                // We don't cache if the object implements ICustomTypeDescriptor. 
-                if (EnableCaching && !(container is ICustomTypeDescriptor))
-                {
-                    PropertyDescriptorCollection properties = null;
-                    Type containerType = container.GetType();
-                    if (!propertyCache.TryGetValue(containerType, out properties))
-                    {
-                        properties = TypeDescriptor.GetProperties(containerType);
-                        propertyCache.TryAdd(containerType, properties);
-                    }
-                    return properties;
-                }
-
-                return TypeDescriptor.GetProperties(container);
-            }
-
-            /// <devdoc>
-            /// </devdoc> 
-            public static object GetPropertyValue(object container, string propName)
-            {
-                if (container == null)
-                {
-                    throw new ArgumentNullException("container");
-                }
-                if (String.IsNullOrEmpty(propName))
-                {
-                    throw new ArgumentNullException("propName");
-                }
-
-                object prop = null;
-
-
-                // get a PropertyDescriptor using case-insensitive lookup 
-                PropertyDescriptor pd = GetPropertiesFromCache(container).Find(propName, true);
-                if (pd != null)
-                {
-                    prop = pd.GetValue(container);
-                }
-                else
-                {
-                    //HttpException(SR.GetString(SR.DataBinder_Prop_Not_Found, container.GetType().FullName, propName));
-                    throw new Exception("Property not found"); 
-                }
-
-                return prop;
-            }
-
-
-            /// <devdoc>
-            /// </devdoc>
-            public static string GetPropertyValue(object container, string propName, string format)
-            {
-                object value = DataBinder.GetPropertyValue(container, propName);
-
-                if ((value == null) || (value == System.DBNull.Value))
-                {
-                    return string.Empty;
-                }
-                else
-                {
-                    if (String.IsNullOrEmpty(format))
-                    {
-                        return value.ToString();
-                    }
-                    else
-                    {
-                        return string.Format(format, value);
-                    }
-                }
-            }
-
-
-            /// <devdoc>
-            /// </devdoc> 
-            public static object GetIndexedPropertyValue(object container, string expr)
-            {
-                if (container == null)
-                {
-                    throw new ArgumentNullException("container");
-                }
-                if (String.IsNullOrEmpty(expr))
-                {
-                    throw new ArgumentNullException("expr");
-                }
-
-                object prop = null;
-                bool intIndex = false;
-
-                int indexExprStart = expr.IndexOfAny(indexExprStartChars);
-                int indexExprEnd = expr.IndexOfAny(indexExprEndChars, indexExprStart + 1);
-
-                if ((indexExprStart < 0) || (indexExprEnd < 0) ||
-                    (indexExprEnd == indexExprStart + 1))
-                {
-                    //throw new ArgumentException(SR.GetString(SR.DataBinder_Invalid_Indexed_Expr, expr));
-                    throw new ArgumentException();
-                }
-
-                string propName = null;
-                object indexValue = null;
-                string index = expr.Substring(indexExprStart + 1, indexExprEnd - indexExprStart - 1).Trim();
-
-                if (indexExprStart != 0)
-                    propName = expr.Substring(0, indexExprStart);
-
-                if (index.Length != 0)
-                {
-                    if (((index[0] == '"') && (index[index.Length - 1] == '"')) ||
-                        ((index[0] == '\'') && (index[index.Length - 1] == '\'')))
-                    {
-                        indexValue = index.Substring(1, index.Length - 2);
-                    }
-                    else
-                    {
-                        if (Char.IsDigit(index[0]))
-                        {
-                            // treat it as a number
-                            int parsedIndex;
-                            intIndex = Int32.TryParse(index, NumberStyles.Integer, CultureInfo.InvariantCulture, out parsedIndex);
-                            if (intIndex)
-                            {
-                                indexValue = parsedIndex;
-                            }
-                            else
-                            {
-                                indexValue = index;
-                            }
-                        }
-                        else
-                        {
-                            // treat as a string 
-                            indexValue = index;
-                        }
-                    }
-                }
-
-                if (indexValue == null)
-                {
-                    //throw new ArgumentException(SR.GetString(SR.DataBinder_Invalid_Indexed_Expr, expr));
-                    throw new ArgumentException();
-                }
-
-                object collectionProp = null;
-                if ((propName != null) && (propName.Length != 0))
-                {
-                    collectionProp = DataBinder.GetPropertyValue(container, propName);
-                }
-                else
-                {
-                    collectionProp = container;
-                }
-
-                if (collectionProp != null)
-                {
-                    Array arrayProp = collectionProp as Array;
-                    if (arrayProp != null && intIndex)
-                    {
-                        prop = arrayProp.GetValue((int)indexValue);
-                    }
-                    else if ((collectionProp is IList) && intIndex)
-                    {
-                        prop = ((IList)collectionProp)[(int)indexValue];
-                    }
-                    else
-                    {
-                        PropertyInfo propInfo =
-                            collectionProp.GetType().GetProperty("Item", BindingFlags.Public | BindingFlags.Instance, null, null, new Type[] { indexValue.GetType() }, null);
-                        if (propInfo != null)
-                        {
-                            prop = propInfo.GetValue(collectionProp, new object[] { indexValue });
-                        }
-                        else
-                        {
-                            //throw new ArgumentException(SR.GetString(SR.DataBinder_No_Indexed_Accessor, collectionProp.GetType().FullName));
-                            throw new ArgumentException();
-                        }
-                    }
-                }
-
-                return prop;
-            }
-
-
-            /// <devdoc>
-            /// </devdoc>
-            public static string GetIndexedPropertyValue(object container, string propName, string format)
-            {
-                object value = DataBinder.GetIndexedPropertyValue(container, propName);
-
-                if ((value == null) || (value == System.DBNull.Value))
-                {
-                    return String.Empty;
-                }
-                else
-                {
-                    if (String.IsNullOrEmpty(format))
-                    {
-                        return value.ToString();
-                    }
-                    else
-                    {
-                        return string.Format(format, value);
-                    }
-                }
-            }
-
-
-            /// <devdoc>
-            /// </devdoc>
-            public static object GetDataItem(object container)
-            {
-                bool foundDataItem;
-                return DataBinder.GetDataItem(container, out foundDataItem);
-            }
-
-
-            /// <devdoc>
-            /// </devdoc>
-            public static object GetDataItem(object container, out bool foundDataItem)
-            {
-                // 
-                if (container == null)
-                {
-                    foundDataItem = false;
-                    return null;
-                }
-
-                // Otherwise, look for a property named "DataItem"
-                string dataItemPropertyName = "DataItem";
-
-                // Check whether the property exists
-                PropertyInfo propInfo = container.GetType().GetProperty(dataItemPropertyName,
-                    BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-
-                // If not, return null 
-                // 
-                if (propInfo == null)
-                {
-                    foundDataItem = false;
-                    return null;
-                }
-
-                // If so, get the value 
-                foundDataItem = true;
-                return propInfo.GetValue(container, null);
-            }
-
-            /// <devdoc> 
-            /// Returns true if the value is null, DBNull, or INullableValue.HasValue is false.
-            /// </devdoc>
-            internal static bool IsNull(object value)
-            {
-                if (value == null || Convert.IsDBNull(value))
-                {
-                    return true;
-                }
-                return false;
+                return ret;
             }
         }
-
     }
 }
