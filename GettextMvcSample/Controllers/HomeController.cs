a1ff27c9efc999a8d7b1a45fe4385820054ee093
Rudi Benkovic <rudi@whiletrue.com>
2012-08-01 16:47:29 +0200

    removed test utf-8 chars...

--- GettextMvcSample/Controllers/HomeController.cs
+++ GettextMvcSample/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace GettextMvcSample.Controllers
 
             var model = new IndexModel();
 
-            model.Message = S._("Hello world from Controller code! čćžđš");
+            model.Message = S._("Hello world from Controller code!");
             model.Email = "";
             model.TooLong = "stringy";
             model.Message2 = S._("Hello {username}!").FormatWith(new {username = "Bob"});
