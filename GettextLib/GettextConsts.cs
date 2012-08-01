5b49f8a7f4ca7d698727333f1d37268c5bcf7589
Rudi Benkovic <rudi@whiletrue.com>
2012-08-01 14:52:02 +0200

    no silly languages here

--- GettextLib/GettextConsts.cs
+++ GettextLib/GettextConsts.cs
@@ -3,7 +3,7 @@
     public static class GettextConsts
     {
         public const string GettextContextKey = "_gettext_context";
-        public const string GettextPseudoLanguage = "sv-SE";
+        public const string GettextPseudoLanguage = "__pseudo__";
         public const string GettextNullLanguage = "__null__";
 		public const string GettextDefaultLanguage = "en-US";
     }
