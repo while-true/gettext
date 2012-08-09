73284949911315945b36f526efee6267ac4cdec9
Rudi Benkovič <rudi@jawmail.org>
2012-08-09 23:34:23 +0200

    Revert "made method names consistent"
    This reverts commit c1f5d646cacd3d3b8d71bbccc30a0f9af6806414.

--- GettextMvcSample/Scripts/index.js
+++ GettextMvcSample/Scripts/index.js
@@ -4,7 +4,7 @@ $(function () {
     $("#index-js-message").text(Gettext._("Hello world from Javascript!"));
 
     for (var i = 0; i < 6; i++) {
-        $("#index-js-message-plural").append(Gettext._("{fileCount} file", "{fileCount} files", i).formatWithNamed({ "fileCount": i }) + "<br/>");
+        $("#index-js-message-plural").append(Gettext._("{fileCount} file", "{fileCount} files", i).formatWith({ "fileCount": i }) + "<br/>");
     }
 
 });
