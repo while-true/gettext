dcdf09261861eb05b59c1248f4b0c58b2055eff3
Rudi Benkovič <rudi@jawmail.org>
2012-10-25 00:54:20 +0200

    search in master view files for strings

--- GettextTools/extract_from_views.bat
+++ GettextTools/extract_from_views.bat
@@ -9,7 +9,7 @@ set __gettext_tools_dir=%~dp0
 
 call %__gettext_tools_dir%extract_vars.bat
 
-%__gettext_tools_dir%find %1 ( -iname "*.cshtml" -o -iname "*.aspx" -o -iname "*.ascx" ) | %__gettext_tools_dir%sed "s/\//\\/g" > %__gettext_current_dir%files.txt
+%__gettext_tools_dir%find %1 ( -iname "*.cshtml" -o -iname "*.aspx" -o -iname "*.ascx" -o -iname "*.Master" ) | %__gettext_tools_dir%sed "s/\//\\/g" > %__gettext_current_dir%files.txt
 %__gettext_tools_dir%xgettext --force-po -f %__gettext_current_dir%files.txt %xgettext_options% -o %__gettext_current_dir%messages-views.pot
 
 DEL %__gettext_current_dir%files.txt
